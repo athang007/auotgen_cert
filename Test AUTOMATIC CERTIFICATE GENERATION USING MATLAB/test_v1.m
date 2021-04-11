8771439803b1fe4b38bd9b28577dc18acc9f68e3
athang007 <48652248+athang007@users.noreply.github.com>
2021-04-12 00:33:45 +0700

    Add files via upload

---
+++ Test AUTOMATIC CERTIFICATE GENERATION USING MATLAB/test_v1.m
@@ -0,0 +1,40 @@
+clc
+clear all
+close all
+home
+
+filename = 'List.xls';
+[num,txt] = xlsread(filename);
+
+len = length(txt);
+
+I = imread('certi.tif');
+
+for i=1:len
+    for j = 2:2
+        text_name(i,j) = txt(i,j);
+    end
+end
+
+for i=1:len
+    for j = 3:3
+        text_team(i,j) = txt(i,j);
+    end
+end
+
+for i=2:len
+    text_all = [text_name(i,2) text_team(i,3)]
+    
+    position = [750 790; 1050 900];
+    
+    RGB = insertText(I,position,text_all,'FontSize',65,'BoxOpacity',0);
+    
+    figure
+    
+    imshow(RGB)
+    
+    y=i-1
+        filename=['Result' num2str(y)]
+        lastf=[filename '.tif']
+        saveas(gcf,lastf)
+end
